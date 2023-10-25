cea92d1ac1ac16f6871ddd588517a80b8efd2d10
Askar Akhmetyanov <122350809+Quarest2@users.noreply.github.com>
2023-10-25 22:37:38 +0300

    Add files via upload

---
+++ ProgramA.cs
@@ -0,0 +1,96 @@
+﻿//The work is done on a macbook :).
+
+using System.Security;
+
+//Method that initializes an array according to the rule.
+static void NewData(ref double[][] A,int N, int M){
+    
+    double n = 1;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            A[i][j] = (Math.Pow(n, 3) / (Math.Pow(n, 2) + 1)) - (3 * Math.Pow(n, 2) / (3 * n - 1));
+            n++;
+        }
+    }
+
+}
+
+int N, M;
+
+while (true)
+{
+    //Telling the user how to end the program.
+    Console.WriteLine("If you want to end the program, type 'out' and click enter, else click enter");
+    if (Console.ReadLine() == "out")
+    {
+        break;
+    }
+
+    //Getting the value of N.
+    Console.WriteLine("Please enter the value of 'N'");
+    while (!int.TryParse(Console.ReadLine(), out N) || N <= 0 || N > 15)
+    {
+        Console.WriteLine("'N' must be a natural number less than 16!");
+    }
+
+    //Getting the value of M.
+    Console.WriteLine("Please enter the value of 'M'");
+    while (!int.TryParse(Console.ReadLine(), out M) || M <= 0 || M > 10)
+    {
+        Console.WriteLine("'M' must be a natural number less than 11!");
+    }
+
+    //Creating the array A.
+    var A = new double[N][];
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = new double[M];
+    }
+
+
+    //Initializing the array A.
+    NewData(ref A, N, M);
+
+    //Getting the name of file.
+    Console.WriteLine("Please enter the name of file");
+    string fileName = Console.ReadLine();
+
+    //Creating the file.
+    string path = $"../../../../{fileName}.txt";
+
+    //Writing N and A in file.
+    try
+    {
+        using (StreamWriter s = File.CreateText(path))
+        {
+            s.WriteLine(N);
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < M; j++)
+                {
+                    if (j != M - 1)
+                    {
+                        s.Write(string.Format("{0:f3}", A[i][j]) + "??");
+                    }
+                    else
+                    { 
+                        s.Write(string.Format("{0:f3}", A[i][j]) + "?");
+                    }
+                }
+
+                s.WriteLine();
+                s.WriteLine();
+            }
+        }
+    }
+    catch (SecurityException)
+    {
+        Console.WriteLine("Sorry, we don't have the permissions to change this file.");
+    }
+    catch (Exception ex)
+    {
+         Console.WriteLine($"Exception while saving: {ex.Message}.");
+    }
+}
---
+++ ProgramB.cs
@@ -0,0 +1,112 @@
+﻿//The work is done on a macbook :).
+
+
+//Method that processing an array according to the rule.
+static void ArrProcessing(double[][] B,ref double[][] res)
+{
+    for (int i = 0; i < B.Length; i ++)
+    {
+        double[] add = new double[B[i].Length / 2];
+        res = res.ToList().Append(add).ToArray();
+        for(int j = 0; j < B[i].Length; j++)
+        {
+            if(j % 2 == 1)
+            {
+                res[i][j/2] = Math.Abs(B[i][j]);
+            }
+        }
+    }
+}
+
+// Method that reading the file and saving it in array.
+static void ArrReading(string path, ref double[][] B)
+{
+    try
+    {
+        //Looking for a file and open it.
+        StreamReader sr = new StreamReader(path);
+        string line = sr.ReadLine();
+
+        //Checking that the file is not empty.
+        if (line == null)
+        {
+            Console.WriteLine("Error: file is empty");
+        }
+
+        //Reading the file line by line and writing the result in B.
+        string[] words;
+        double[] numbers;
+        int len;
+        while (line != null)
+        {
+            words = line.Split('?', StringSplitOptions.RemoveEmptyEntries);
+            len = words.Length;
+            numbers = new double[len];
+            for (int i = 0; i < len; i++)
+            {
+                if (!double.TryParse(words[i], out numbers[i]))
+                {
+                    Console.WriteLine("Error: incorrect data provided");
+                    B = new double[0][];
+                    return;
+                }
+            }
+            B = B.ToList().Append(numbers).ToArray();
+            line = sr.ReadLine();
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Exception: " + e.Message);
+    }
+}
+
+
+
+while (true)
+{
+    //Telling the user how to end the program.
+    Console.WriteLine("If you want to end the program, type 'out' and click enter, else click enter");
+    if (Console.ReadLine() == "out")
+    {
+        break;
+    }
+
+    double[][] B = new double[0][];
+
+    // Reading the file and checking that it have right values.
+    while (B.Length == 0)
+    {
+        Console.WriteLine("Please enter the name of file");
+        string fileName = Console.ReadLine();
+        string path = $"../../../../{fileName}.txt";
+        ArrReading(path, ref B);
+    }
+
+    //Processing B using the method.
+    double[][] res = new double[0][];
+    ArrProcessing(B, ref res);
+
+
+    //Output the array before and after proccessing.
+    Console.WriteLine();
+    Console.WriteLine("Before:");
+    foreach (double[] i in B)
+    {
+        foreach (double j in i)
+        {
+            Console.Write(string.Format("{0:f3}", j) + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("After:");
+    foreach (double[] i in res)
+    {
+        foreach (double j in i)
+        {
+            Console.Write(string.Format("{0:f3}", j) + " ");
+        }
+        Console.WriteLine();
+    }
+}
